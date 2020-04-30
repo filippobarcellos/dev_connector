76ac4726dda55b3fe85ae3d190c9141575f5c087
Filippo Barcellos <barcellos.filippo@gmail.com>
2020-04-30 15:28:46 +0200

    [backend] - add cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import connectDB from './config/database';
 import routes from './routes';
 
@@ -6,6 +7,7 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
